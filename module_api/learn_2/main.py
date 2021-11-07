b6ee432271bc29493a6ba8d04e6fd604b2b8d019
lilu <23082010qQ>
2021-11-07 19:15:03 +0300

    Deleted the extra package import Gave the correct names to the variables

--- module_api/learn_2/main.py
+++ module_api/learn_2/main.py
@@ -4,36 +4,35 @@ import json
 from pip import main
 from urllib.parse import urlparse
 from dotenv import load_dotenv
-from dotenv import dotenv_values
 
-url_info_bitlink = 'https://api-ssl.bitly.com/v4/bitlinks/'
-domain_bitlink = 'bit.ly'
+URL_INFO_BITLINK = 'https://api-ssl.bitly.com/v4/bitlinks/'
+DOMAIN_BITLINK = 'bit.ly'
 
 def main_func():
-    TOKEN = os.getenv("TOKEN")
-    headers = {'Authorization': f'Bearer {TOKEN}'}
+    token = os.getenv("BITLINK_TOKEN")
+    headers = {'Authorization': f'Bearer {token}'}
     user_input_link = input('Введите ссылку: ')
     parse_link = urlparse(user_input_link)
     domain = parse_link.netloc
     url_path = parse_link.path
-    flag = domain_bitlink == domain
+    flag = DOMAIN_BITLINK == domain
 
     def shorten_link(token, url):
-        response = requests.post(url_info_bitlink, headers=headers, json={'long_url': user_input_link})
+        response = requests.post(URL_INFO_BITLINK, headers=headers, json={'long_url': user_input_link})
         response.raise_for_status()
         answer = response.json()
         short_link = answer['link']
         return short_link
 
     def count_clicks(token, url):
-        url_count_clicks_concat = f'{url_info_bitlink}{domain}{url_path}/clicks'
+        url_count_clicks_concat = f'{URL_INFO_BITLINK}{domain}{url_path}/clicks'
         response_clicks = requests.get(url_count_clicks_concat, headers=headers)
         response_clicks.raise_for_status()
         response_clicks_answer = response_clicks.json()
         return response_clicks_answer
 
     def is_bitlink(token, url):
-        url_is_bitlink_concat = f'{url_info_bitlink}{domain}{url_path}'
+        url_is_bitlink_concat = f'{URL_INFO_BITLINK}{domain}{url_path}'
         response_is_bitlink = requests.get(url_is_bitlink_concat, headers=headers)
         response_is_bitlink.raise_for_status()
         response_is_bitlink_answer = response_is_bitlink.json()
@@ -41,12 +40,12 @@ def main_func():
 
     try:
         if flag:
-            info_count_clicks = count_clicks(TOKEN, user_input_link)
+            info_count_clicks = count_clicks(token, user_input_link)
             print(info_count_clicks)
-            info_is_bitlink = is_bitlink(TOKEN, user_input_link)
+            info_is_bitlink = is_bitlink(token, user_input_link)
             print(info_is_bitlink)
         else:
-            bitlink = shorten_link(TOKEN, user_input_link)
+            bitlink = shorten_link(token, user_input_link)
             print(bitlink)
     except requests.exceptions.HTTPError:
         print('Вы ввели ссылку некорректно!')
