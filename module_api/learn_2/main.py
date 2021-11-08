c8ca801fab001b06a777d02cc142a49cab2d0635
lilu <23082010qQ>
2021-11-08 08:32:14 +0300

    I broke the code of the composite functions into parts so that each function could be used separately from the others.

--- module_api/learn_2/main.py
+++ module_api/learn_2/main.py
@@ -6,51 +6,49 @@ from dotenv import load_dotenv
 URL_INFO_BITLINK = 'https://api-ssl.bitly.com/v4/bitlinks/'
 DOMAIN_BITLINK = 'bit.ly'
 
+user_input_link = input('Введите ссылку: ')
+parse_link = urlparse(user_input_link)
+domain = parse_link.netloc
+url_path = parse_link.path
+flag = DOMAIN_BITLINK == domain
+
+def shorten_link(token, url):
+    response = requests.post(URL_INFO_BITLINK, headers=headers, json={'long_url': user_input_link})
+    response.raise_for_status()
+    answer = response.json()
+    short_link = answer['link']
+    return short_link
+
+def count_clicks(token, url):
+    url_count_clicks_concat = f'{URL_INFO_BITLINK}{domain}{url_path}/clicks'
+    response_clicks = requests.get(url_count_clicks_concat, headers=headers)
+    response_clicks.raise_for_status()
+    response_clicks_answer = response_clicks.json()
+    return response_clicks_answer
+
+def is_bitlink(token, url):
+    url_is_bitlink_concat = f'{URL_INFO_BITLINK}{domain}{url_path}'
+    response_is_bitlink = requests.get(url_is_bitlink_concat, headers=headers)
+    response_is_bitlink_answer = response_is_bitlink.ok
+    return response_is_bitlink_answer
+
 def main_func():
-    token = os.getenv("BITLINK_TOKEN")
-    headers = {'Authorization': f'Bearer {token}'}
-    user_input_link = input('Введите ссылку: ')
-    parse_link = urlparse(user_input_link)
-    domain = parse_link.netloc
-    url_path = parse_link.path
-    flag = DOMAIN_BITLINK == domain
-
-    def shorten_link(token, url):
-        response = requests.post(URL_INFO_BITLINK, headers=headers, json={'long_url': user_input_link})
-        response.raise_for_status()
-        answer = response.json()
-        short_link = answer['link']
-        return short_link
-
-    def count_clicks(token, url):
-        url_count_clicks_concat = f'{URL_INFO_BITLINK}{domain}{url_path}/clicks'
-        response_clicks = requests.get(url_count_clicks_concat, headers=headers)
-        response_clicks.raise_for_status()
-        response_clicks_answer = response_clicks.json()
-        return response_clicks_answer
-
-    def is_bitlink(token, url):
-        url_is_bitlink_concat = f'{URL_INFO_BITLINK}{domain}{url_path}'
-        response_is_bitlink = requests.get(url_is_bitlink_concat, headers=headers)
-        response_is_bitlink.raise_for_status()
-        response_is_bitlink_answer = response_is_bitlink.json()
-        return response_is_bitlink_answer
-
     try:
+        info_is_bitlink = is_bitlink(token, user_input_link)
+        print(info_is_bitlink)
         if flag:
             info_count_clicks = count_clicks(token, user_input_link)
             print(info_count_clicks)
-            info_is_bitlink = is_bitlink(token, user_input_link)
-            print(info_is_bitlink)
         else:
             bitlink = shorten_link(token, user_input_link)
             print(bitlink)
     except requests.exceptions.HTTPError:
         print('Вы ввели ссылку некорректно!')
 
-
 if __name__ == '__main__':
     load_dotenv()
+    token = os.getenv("BITLINK_TOKEN")
+    headers = {'Authorization': f'Bearer {token}'}
     main_func()
 
 
