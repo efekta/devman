afd04ea673df4a9eac36312262929660609f005b
lilu <23082010qQ>
2021-11-17 20:31:34 +0300

    Designed the image download as a function New file .gitignore for this task

--- module_api/learn_4/main.py
+++ module_api/learn_4/main.py
@@ -5,11 +5,17 @@ url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 img_file = 'hubble.jpeg'
 
 path = Path('images').mkdir(parents=True, exist_ok=True)
-response = requests.get(url)
-response.raise_for_status()
-with open(f'images/{img_file}', 'wb') as file:
-    file.write(response.content)
 
+def upload_img(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(f'images/{img_file}', 'wb') as file:
+        file.write(response.content)
+        img = file
+    return img
+
+upload_img(url, path)
 
 
 
