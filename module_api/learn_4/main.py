42a1e8be77cb723ef80439a7bc86780ef6e8275f
lilu <23082010qQ>
2021-11-20 17:13:11 +0300

    I brought the links received from the SpaceX API to the terminal.

--- module_api/learn_4/main.py
+++ module_api/learn_4/main.py
@@ -6,18 +6,24 @@ url = 'https://api.spacexdata.com/v4/launches'
 Path('images').mkdir(parents=True, exist_ok=True)
 img_file = 'hubble.jpeg'
 path = f'images/{img_file}'
+item_links = []
 
-def upload_img(url, path):
+def upload_img(url):
     response = requests.get(url)
     response.raise_for_status()
+    response = response.json()
+    for item in response:
+        link_item = item['links']
+        item_links.append(link_item['patch'])
 
-    with open(path, 'wb') as file:
-        file.write(response.content)
-        img = file
-    return img
+    # with open(path, 'wb') as file:
+    #     file.write(response.content)
+    #     img = file
+    # return img
 
+upload_img(url)
+print(item_links)
 
-upload_img(url, path)
 
 
 
